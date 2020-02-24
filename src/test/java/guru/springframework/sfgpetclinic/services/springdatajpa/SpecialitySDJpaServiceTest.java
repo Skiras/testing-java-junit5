5ac5d9453c52fa3892a205acf22fe4157a1f1952
Anton Bodyakin <anton_bodyakin@epam.com>
2020-02-24 21:00:54 +0300

    adding mockito examples

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -0,0 +1,109 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class SpecialitySDJpaServiceTest {
+
+    @Mock
+    private SpecialtyRepository specialtyRepository;
+
+    @InjectMocks
+    private SpecialitySDJpaService service;
+
+    @Test
+    void testDeleteByObject() {
+        // given
+        Speciality speciality = new Speciality();
+
+        // when
+        service.delete(speciality);
+
+        // then
+        then(specialtyRepository).should().delete(any(Speciality.class));
+    }
+
+    @Test
+    void findByIdTest() {
+        // given
+        Speciality speciality = new Speciality();
+
+        given(specialtyRepository.findById(1L)).willReturn(Optional.of(speciality));
+
+        // when
+        Speciality actualSpeciality = service.findById(1L);
+
+        // then
+        assertThat(actualSpeciality).isNotNull();
+        then(specialtyRepository).should().findById(anyLong());
+        then(specialtyRepository).shouldHaveNoMoreInteractions();
+    }
+
+    @Test
+    void deleteById() {
+        // given - none
+
+        // when
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        // then
+        then(specialtyRepository).should(times(2)).deleteById(1L);
+    }
+
+    @Test
+    void deleteByIdAtLeast() {
+        // given - none
+
+        // when
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        // then
+        then(specialtyRepository).should(atLeastOnce()).deleteById(1L);
+    }
+
+    @Test
+    void deleteByIdAtMost() {
+        // given - none
+
+        // when
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        // then
+        then(specialtyRepository).should(atMost(5)).deleteById(1L);
+    }
+
+    @Test
+    void deleteByIdNever() {
+        // when
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        // then
+        then(specialtyRepository).should(never()).deleteById(5L);
+    }
+
+    @Test
+    void delete() {
+        // when
+        service.delete(new Speciality());
+
+        // then
+        then(specialtyRepository).should().delete(any());
+    }
+}
---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaServiceTest.java
@@ -0,0 +1,72 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.repositories.VisitRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+class VisitSDJpaServiceTest {
+
+    @Mock
+    private VisitRepository visitRepository;
+
+    @InjectMocks
+    private VisitSDJpaService service;
+
+    @Test
+    void findAll() {
+        given(visitRepository.findAll()).willReturn(Set.of(new Visit()));
+
+        Set<Visit> actualVisits = service.findAll();
+
+        then(visitRepository).should().findAll();
+        assertThat(actualVisits).isNotEmpty();
+    }
+
+    @Test
+    void findById() {
+        given(visitRepository.findById(anyLong())).willReturn(Optional.of(new Visit()));
+
+        Visit actualVisit = service.findById(1L);
+
+        then(visitRepository).should().findById(anyLong());
+        assertThat(actualVisit).isNotNull();
+    }
+
+    @Test
+    void save() {
+        given(visitRepository.save(any(Visit.class))).willReturn(new Visit());
+
+        Visit savedVisit = service.save(new Visit());
+
+        then(visitRepository).should().save(any(Visit.class));
+        assertThat(savedVisit).isNotNull();
+    }
+
+    @Test
+    void delete() {
+        service.delete(new Visit());
+
+        then(visitRepository).should().delete(any(Visit.class));
+    }
+
+    @Test
+    void deleteById() {
+        service.deleteById(1L);
+
+        then(visitRepository).should().deleteById(anyLong());
+    }
+}
